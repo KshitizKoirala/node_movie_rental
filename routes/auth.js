78cfe347ed05a629dc8144bae873027f3bff713e
Kshitiz <48265967+KshitizKoirala@users.noreply.github.com>
2020-11-11 16:25:12 +0545

    Movie Rental App

---
+++ routes/auth.js
@@ -0,0 +1,38 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const {User} = require('../models/user');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const Joi = require('joi');
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('Invalid email or password');
+    
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid Email or password');
+    
+    const token = user.generateAuthToken();
+    res.send(token);
+
+
+});
+
+
+
+function validate(req){
+    const schema = {
+        email : Joi.string().required().email(),
+        password: Joi.string().required()
+    }
+    return Joi.validate(req, schema)
+}
+
+module.exports = router;
